d121209d33f9fb5ca2a25ec9183f9976445db6b5
BigHuni <hdh8659@gmail.com>
2022-07-28 11:04:48 +0900

    [add] 연산자 실습 파일 추가

---
+++ 2022-07-28[자바 기초]/OperatorEx2.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 10 % 3;
+		
+		System.out.println(num);
+	}
+
+}
