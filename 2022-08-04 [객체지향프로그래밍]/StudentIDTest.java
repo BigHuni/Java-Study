28e6c089403d837d79d127695342ba6696362c45
BigHuni <hdh8659@gmail.com>
2022-08-05 04:32:47 +0900

    [add] 2022-08-04 스터디 일지 추가

---
+++ 2022-08-04 [객체지향프로그래밍]/StudentIDTest.java
@@ -0,0 +1,17 @@
+package staticex;
+
+public class StudentIDTest {
+
+	public static void main(String[] args) {
+
+		Student studentHeo = new Student("Heo");
+		System.out.println(Student.getSerialNum());		// 1001
+		
+		Student studentKim = new Student("Kim");
+		System.out.println(Student.getSerialNum());		// 1002
+		System.out.println(Student.getSerialNum());		// 1002
+		
+		System.out.println(studentHeo.getStudentID());	// 1001
+		System.out.println(studentKim.getStudentID());	// 1002
+	}
+}
